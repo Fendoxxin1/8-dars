4872a50e2234633c8836a72f5693f682c9c8a0f1
Fendoxxin1 <fendoxxindehlaviy@gmail.com>
2024-09-17 16:12:45 +0500

    1-masala ishlandi

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print("Hello world")
+lst = ["A", "B", "C", "D", "E", "F", "G", "L", "Q", "U"]
+n = int(input("Son kiriting: "))
+
+result = []
+
+for i in range(0, len(lst), n):
+    result.append(lst[i:i+n])
+
+print(result)
